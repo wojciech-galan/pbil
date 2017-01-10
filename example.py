12324bf0ba9709f0468231ea0f42e5a8265e2471
wojciech-galan <wojciech.galan@gmail.com>
2017-01-10 11:01:23 +0100

    usage example

---
+++ example.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import functools
+import numpy as np
+
+from pbil.optimizer import optimize
+
+if __name__ == '__main__':
+    def eval_fun(nums, bits):
+        assert len(nums) == len(bits)
+        return sum(nums[i]*bits[i] for i in range(len(nums)))
+    data = [-1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13, 14, 15, 16, -17, -18, -19]
+    l = []
+    print optimize(0.02, 0.02, 10, 2, 2, len(data), 25, functools.partial(eval_fun, data), vec_storage=l)
+    print '--------------------------------------------------------------------------'
+    print np.asarray(l, float)
