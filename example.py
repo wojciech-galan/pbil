18103958e5db735902d43841a3822338dfa9e688
wojciech-galan <wojciech.galan@gmail.com>
2017-01-13 19:16:28 +0100

    output cleaned

--- example.py
+++ example.py
@@ -13,5 +13,6 @@ if __name__ == '__main__':
     data = [-1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13, 14, 15, 16, -17, -18, -19]
     l = []
     print optimize(0.02, 0.02, 10, 2, 2, len(data), 25, functools.partial(eval_fun, data), vec_storage=l)
-    print '--------------------------------------------------------------------------'
-    print np.asarray(l, float)
+    # if you're interested in the evolution of the population vector, uncomment the lines below
+    # print '--------------------------------------------------------------------------'
+    # print np.asarray(l, float)
