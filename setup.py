8c7934eda21aadbafa2cd3fe816cf74e5cf220bb
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2017-01-31 10:52:21 +0100

    changed package name

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ from setuptools import setup, find_packages
 # versioning https://packaging.python.org/distributing/#semantic-versioning-preferred
 
 setup(
-    name="PBIL",
+    name="pbil",
     version='0.1.0',
     description='Population-based incremental learning in Python',
-    url='https://github.com/wojciech-galan/PBIL',
+    url='https://github.com/wojciech-galan/pbil',
     author='Wojciech Gałan',
     license='GNU GPL v3.0',
     install_requires=[
         'numpy'
     ],
-    packages=find_packages()
+    packages=['pbil']
 )
