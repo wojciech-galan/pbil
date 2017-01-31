e4e83979a2cca03a922af12b35b7cd0d8e5b1900
wojciech-galan <wojciech.galan@gmail.com>
2017-01-31 08:19:54 +0100

    initial setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# requirements https://caremad.io/posts/2013/07/setup-vs-requirement/
+# and https://packaging.python.org/requirements/
+# versioning https://packaging.python.org/distributing/#semantic-versioning-preferred
+
+setup(
+    name="PBIL",
+    version='0.1.0',
+    install_requires=[
+        'numpy'
+    ]
+)
